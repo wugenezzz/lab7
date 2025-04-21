8cf242f3af57e353ea7c11b00d3feae67ae6cecf
wugenezzz <wugenewin@gmail.com>
2025-04-21 21:28:36 +0800

    fuck

--- src/Cylinder.cpp
+++ src/Cylinder.cpp
@@ -1,29 +1,4 @@
-# ifndef CYLINDER_CPP
-# define CYLINDER_CPP
 
-# include "Cylinder.h"
-#include <math.h>
-#include <iomanip>
-
-#define _USE_MATH_DEFINES
-# define pi 3.1415926535897932384626433
-double Cylinder :: SurfaceArea(){
-    double ans=0;
-    ans=pi*radius*2*(radius+height);//pi*2*pow(radius, 2)+pi*2*radius*height;
-    return ans;
-}
-
-double Cylinder :: Volume(){
-    double ans=0;
-    ans=pi*radius*radius*height;
-    return ans;
-}
-
-double Cylinder :: Circumference(){
-    double ans=0;
-    ans=pi*radius*2;
-    return ans;
-}
 
 istream & operator>>(istream & in, Cylinder & cldr)
 {
